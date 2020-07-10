29162db5b509d6b34bb278e9c2297b42a0df6967
Aditi Prakash <aditiprakash@MacBook-Pro.local>
2020-07-10 14:34:46 -0600

    complete business card

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,45 +12,84 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return  MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-            Container(
-            width: 100.0,
-              color: Colors.red,
-            ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-//                    margin: const EdgeInsets.all(10.0),
-                    color: Colors.yellow,
-                    width: 100.0,
-                    height: 100.0,
-                  ),
-                  Container(
-//                    margin: const EdgeInsets.all(10.0),
-                    color: Colors.green,
-                    width: 100.0,
-                    height: 100.0,
-                  ),
-                ],
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+             CircleAvatar(
+                backgroundImage: AssetImage('images/display.png'),
+                radius: 80.0,
               ),
-//              SizedBox(
-//                width: 210.0,
-//              ),
-            Container(
-              width: 100.0,
-              color: Colors.blue,
-            )
-            ],
+              Text(
+                  'Aditi',
+              style: TextStyle(
+                fontFamily: 'Pacifico',
+                fontSize: 40.0,
+                color: Colors.teal,
+//                fontWeight: FontWeight.bold,
+              ),
+              ),
+            Text(
+              'SOFTWARE ENGINEER',
+              style: TextStyle(
+                fontFamily: 'SourceSans',
+                fontSize: 20.0,
+                color: Colors.teal[300],
+                letterSpacing: 2.5,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(child: Divider(
+              color: Colors.teal[100],
+            ),
+              width:150.0 ,
+              height: 20.0,),
+            Card(
+              color: Colors.teal[100],
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: Padding(
+                padding: const EdgeInsets.all(6.0),
+                child: ListTile(
+                  leading:
+                      Icon(
+                        Icons.email,
+                        color: Colors.teal,
+                      ),
+                      title: Text(
+                        'ap2205@gmail.com',
+                        style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontFamily: 'Source Sans Pro',
+                          fontSize: 20.0,
+                        ),
+                      ),
+                )
+              ),
+            ),
+
+            Card(
+              color: Colors.teal[100],
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: Padding(
+                padding: const EdgeInsets.all(6.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.location_city,
+                    color: Colors.teal,
+                  ) ,
+                  title: Text(
+                      'Boulder',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20.0,
+                      ),
+                )
+              ))),
+              ],
+            )),
           ),
-        ),
-      ),
-    );
+        );
   }
 }
-
